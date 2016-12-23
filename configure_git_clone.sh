d824a6d74132b7aca6d5deb2bb1a96db41d0dd14
Christopher Beck <chris@mail.chrisbeck.co>
2016-12-23 07:35:10 +0000

    add git clone and rebasing script

--- configure_git_clone.sh
+++ configure_git_clone.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
+set -e
+set -x
 
 mkdir git
 cd git
 git clone https://github.com/cbeck88/cegui-mirror-two
-
 cd cegui-mirror-two
-git checkout v0-8+travis
-git pull --rebase v08
-git push origin v0-8+travis
-
-git checkout default+travis
-git pull --rebase def
-git push origin default+travis
+git remote set-url origin git@github.com:cbeck88/cegui-mirror-two.git
--- rebase_git.sh
+++ rebase_git.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+set -e
+set -x
 
 echo "git rebase travis branches"
 
 cd git/cegui-mirror-two
 
-git checkout v0-8+travis
-git pull --rebase v08
-git push origin v0-8+travis
+git fetch origin def v08 v0-8+travis default+travis
 
+git checkout v08
+git checkout v0-8+travis
+git pull origin
+git rebase refs/heads/v08
+git push -f origin v0-8+travis
+
+git checkout def
 git checkout default+travis
-git pull --rebase def
-git push origin default+travis
+git pull origin
+git rebase refs/heads/def
+git push -f origin default+travis
