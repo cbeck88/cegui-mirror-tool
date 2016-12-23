881cd68b35495c921d40b16e57a725597975ad4f
Chris Beck <beck.ct@gmail.com>
2016-12-23 18:23:31 +0000

    make rebase_git.sh more robust
    always reset hard to remote content before attempting to rebase anything

--- rebase_git.sh
+++ rebase_git.sh
@@ -9,13 +9,16 @@ cd git/cegui-mirror-two
 git fetch origin def v08 v0-8+travis default+travis
 
 git checkout v08
+git reset --hard origin/v08
 git checkout v0-8+travis
-git pull origin
+git reset --hard origin/v0-8+travis
 git rebase refs/heads/v08
 git push -f origin v0-8+travis
 
 git checkout def
+git reset --hard origin/def
 git checkout default+travis
+git reset --hard origin/default+travis
 git pull origin
 git rebase refs/heads/def
 git push -f origin default+travis
